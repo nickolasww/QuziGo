e7fde25cd9d232455d5930c8b91252aa0cefff8e
Ww <nickolaswewe2@gmail.com>
2025-10-29 19:06:37 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen flex flex-col justify-center items-center bg-black'> 
+        <h1 className='text-white text-4xl font-bold'>Comming Soon</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
