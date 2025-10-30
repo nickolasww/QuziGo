ff4e4fb17bed51da8197fbf76c845d28b80b2948
your_github_username <your_github_email>
2025-10-31 02:17:44 +0700

    fix: header homepage and button color

--- src/pages/components/navbar/navbar.tsx
+++ src/pages/components/navbar/navbar.tsx
@@ -3,16 +3,16 @@ import { Button } from "@/components/ui/button"
 
 const Navbar = () => {
   return (
-    <div className='flex w-full justify-between items-center bg-white px-20 py-5'>
-      <h1>
+    <div className='flex w-full justify-between items-center bg-black border-b border-gray-700 px-20 py-5'>
+      <h1 className='text-white text-2xl font-bold'>
         QuziGo
       </h1>
       <div className='flex gap-5'> 
        <a href='/auth/signin'> 
-        <Button variant={'outline'}>Sign In</Button>
+        <Button variant={'outline'} className='text-black text-md'>Sign In</Button>
         </a>
        <a href='/auth/register'>
-        <Button variant={'outline'}>Register</Button>
+        <Button variant={'gradient'} className='text-white text-md'>Register</Button>
        </a>
       </div>
 
--- src/pages/homepage/partials/header.tsx
+++ src/pages/homepage/partials/header.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Orange from "../../../assets/Orange.svg";
+import Atas from "../../../assets/Atas.svg";
+import Bawah from "../../../assets/Bawah.svg";
+import { BsStars } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div className='flex flex-col gap-9 w-full justify-center items-center h-screen bg-black px-20 py-5'>
-      <h1 className='text-white text-7xl font-bold'> 
-        Learn, Quiz, Earn Rewards
+    <div className="flex flex-col w-full justify-center items-center h-screen bg-black px-20 py-5">
+      <div className="relative w-screen">
+        <img src={Atas} alt="QuziGo Logo" className="absolute  -bottom-17" />
+      </div>
+      <div className=" flex gap-2 items-center text-white px-3 py-px rounded-lg bg-[#202022] mb-3">
+        <BsStars className="text-purple-700" />
+        <h1>The ultimate quiz experience</h1>
+      </div>
+      <h1 className="text-white text-7xl font-bold mb-9">
+        Learn, Quiz,{" "}
+        <span className="bg-linear-to-r from-[#5813C1] to-[#C45037] bg-clip-text text-transparent">
+          Earn Rewards
+        </span>
       </h1>
-      <h2 className='text-white text-center text-lg font-normal px-132'> 
-        Join thousands of students and teachers on the ultimate quiz platform. Test your knowledge, compete with peers, and win exciting rewards
+      <h2 className="text-white text-center text-lg font-normal px-132 mb-9">
+        Join thousands of students and teachers on the ultimate quiz platform.
+        Test your knowledge, compete with peers, and win exciting rewards
       </h2>
       <div>
-        <Button variant={'destructive'} className='text-white'>Get Started</Button>
+        <Button
+          variant={"gradient"}
+          className="text-md cursor-pointer text-white"
+          size={"lg"}
+        >
+          Get Started
+        </Button>
+      </div>
+      <div className="relative w-screen">
+        <img src={Bawah} alt="QuziGo Logo" className="absolute -top-17" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
